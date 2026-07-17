3425bcf9694e1c2dd9c4c3037c07b596fd1afb72
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2026-07-17 16:24:26 +0500

    move events to realtime

--- src/routes/Realtime.js
+++ src/routes/Realtime.js
@@ -1,17 +1,60 @@
+const express = require('express');
+const router = express.Router();
+
+const { resp } = require('../func/misc');
+const { sseClients } = require('../func/sse');
+
+const Shop = require('../models/Shop');
+(async () => await Shop.updateMany({}, { $set: { isOnline: false } }))();
+
 // -------------------------------------------------------------------------- //
 
-// const { sseClients } = require('../func/sse');
+router.get('/events', async (req, res) => {
+  if (!req.token.sid) return resp(res, 403, 'forbidden');
 
-// router.patch('/:shopId/isOnline', validateObjectIds('shopId'), async (req, res) => {
-//   if (!req.token.sid || req.token.sid !== req.params.shopId) return resp(res, 403, 'forbidden');
-//   if (!sseClients.get(req.token.sid)) return resp(res, 400, 'shop must be connected to sse to update');
+  sseClients.set(req.token.sid, res);
 
-//   const shop = await Shop.findByIdAndUpdate(
-//     req.params.shopId,
-//     { isOnline: true, lastSeen: new Date() },
-//     { returnDocument: 'after' }
-//   );
+  res.set({
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache',
+    'Content-Type': 'text/event-stream',
+  });
 
-//   if (!shop) return resp(res, 404, 'not found');
-//   return resp(res, 200, 'isOnline updated', { shop });
-// });
+  res.flushHeaders();
+  res.write(`event: connected\ndata: \n\n`);
+
+  const ping = setInterval(async () => {
+    const shop = await Shop.findById(req.token.sid);
+
+    if (shop.lastSeen < (Date.now() - 10000)) {
+      shop.isOnline = false;
+      await shop.save();
+    };
+
+    res.write('event: ping\ndata: \n\n');
+  }, 5000);
+
+  req.on('close', async () => {
+    clearInterval(ping);
+    sseClients.delete(req.token.sid);
+    await Shop.findByIdAndUpdate(req.token.sid, { isOnline: false });
+  });
+});
+
+router.patch('/status/:shopId', validateObjectIds('shopId'), async (req, res) => {
+  if (!req.token.sid || req.token.sid !== req.params.shopId) return resp(res, 403, 'forbidden');
+  if (!sseClients.get(req.token.sid)) return resp(res, 400, 'shop must be connected to sse to update');
+
+  const shop = await Shop.findByIdAndUpdate(
+    req.params.shopId,
+    { isOnline: true, lastSeen: new Date() },
+    { returnDocument: 'after' }
+  );
+
+  if (!shop) return resp(res, 404, 'not found');
+  return resp(res, 200, 'isOnline updated', { shop });
+});
+
+// -------------------------------------------------------------------------- //
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -77,13 +77,11 @@ app.get('/health', async (req, res) => {
 
 app.use('/api/auth', require('./routes/Auth.js'));
 app.use('/api/files', require('./routes/Files.js'));
-
 app.use('/api/jobs', jwtAuth, require('./routes/Jobs.js'));
 app.use('/api/shops', jwtAuth, require('./routes/Shops.js'));
 app.use('/api/stats', jwtAuth, require('./routes/Stats.js'));
 app.use('/api/users', jwtAuth, require('./routes/Users.js'));
 app.use('/api/drafts', jwtAuth, require('./routes/Drafts.js'));
-app.use('/api/events', jwtAuth, require('./routes/Events.js'));
 app.use('/api/topups', jwtAuth, require('./routes/Topups.js'));
 app.use('/api/history', jwtAuth, require('./routes/History.js'));
 app.use('/api/profile', jwtAuth, require('./routes/Profile.js'));
